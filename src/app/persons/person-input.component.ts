110c669278974ea62fbd609658f959dbccf330ed
Marli <marleysumarly@gmail.com>
2023-04-13 12:58:24 +0700

    User input

---
+++ src/app/persons/person-input.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-person-input',
+  templateUrl: './app-input.component.html',
+  styleUrls: ['./app-input.component.scss']
+})
+
+export class PersonInputComponent { }
